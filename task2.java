575338cd82646e0e9c2ce05e7a0fe23130a68781
Subim Gautam <subimgautam@gmail.com>
2024-04-15 13:11:01 +0545

    Task

---
+++ task2.java
@@ -0,0 +1,6 @@
+public class task3{ 
+    public static void main(String[] args) {
+        System.out.println("This is my first program");
+        System.out.println("I am on module ST4003CEM");
+        }
+    }
---
+++ variable.java
@@ -0,0 +1,19 @@
+public class variable {
+    public static void main(String[] args) {
+        /* Non primitive data type */
+        /*Declarative */
+        byte bytevalue;
+        /*Initialization */
+        bytevalue = 100;
+        /* Declaration and initialization */
+        short shortvalue=200;
+        /* Multiple declaration */
+        int value1, intValue2;
+        int Value1 = 10000;
+        intValue2 = 2000;
+        /*Multiple declaration and initiation */
+        long longVal1 = -9000, longval2 = 9000;
+        float floatValue = 90.90f; // the letter 'f' is needed
+        double doublevalue = 100.897d; // the letter 'd' is optional
+    }
+}
