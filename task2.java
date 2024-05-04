37e0ded4d8f113f6b4d04c3c8cac01a95198c71e
Subim Gautam <subimgautam@gmail.com>
2024-05-04 16:46:54 +0545

    OOP Task

--- task2.java
+++
@@ -1,6 +0,0 @@
-public class task2{ 
-    public static void main(String[] args) {
-        System.out.println("This is my first program");
-        System.out.println("I am on module ST4003CEM");
-        }
-    }
